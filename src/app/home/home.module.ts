0c5d91fe57b452303839272e531f1c4f5a28457e
cfho <chengfeng.ho@gmail.com>
2021-11-07 16:16:51 +0800

    range selection in modal

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,4 +1,8 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt, 'zh-Hant');
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
@@ -18,6 +22,7 @@ import { CalendarModule } from 'ion2-calendar';
     HomePageRoutingModule,
     CalendarModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  providers: [{ provide: LOCALE_ID, useValue: 'zh-Hant' }]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,27 +1,47 @@
 import { Component } from '@angular/core';
-import { CalendarComponentOptions } from 'ion2-calendar';
+import {
+  CalendarComponentOptions,
+  CalendarModal,
+  CalendarModalOptions,
+} from 'ion2-calendar';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-
 export class HomePage {
   type: 'string'; // 'string' | 'js-date' | 'moment' | 'time' | 'object'
-  
+  dateRange: { from: string; to: string };
   dateMulti: string[];
+
   optionsMulti: CalendarComponentOptions = {
-    pickMode: 'multi'
+    pickMode: 'multi',
   };
 
-
-  dateRange: { from: string; to: string; };
   optionsRange: CalendarComponentOptions = {
-    pickMode: 'range'
+    monthFormat: 'YYYY 年 MM 月 ',
+    weekdays: ['天', '一', '二', '三', '四', '五', '六'],
+    weekStart: 1,
+    // defaultDate: {new Date()}
   };
 
-  constructor() { }
+  async calendarModal() {
+    const options: CalendarModalOptions = {
+      pickMode: 'range',
+      title: 'Range Date',
+    };
+
+    let calendarUi = await this.modalController.create({
+      component: CalendarModal,
+      componentProps: { options },
+    });
+
+    calendarUi.present();
+  }
+
+  constructor(public modalController: ModalController) {}
 
   onChange($event) {
     console.log($event);
