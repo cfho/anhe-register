8c5edfc1f6ab2c068ad5ef32ad7698ab56a093ca
cfho <cfho@users.noreply.github.com>
2021-11-08 18:24:15 +0800

    add toolbar icon

--- src/app/home/person-list/add-list/add-list.page.ts
+++ src/app/home/person-list/add-list/add-list.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  CalendarComponentOptions,
+  CalendarModal,
+  CalendarModalOptions,
+} from 'ion2-calendar';
+import { ModalController } from '@ionic/angular';
 
 
 @Component({
@@ -6,10 +12,44 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './add-list.page.html',
   styleUrls: ['./add-list.page.scss'],
 })
-export class AddListPage implements OnInit {
-  constructor() { }
+export class AddListPage {
+  type: 'string'; // 'string' | 'js-date' | 'moment' | 'time' | 'object'
+  dateRange: { from: string; to: string };
+  dateMulti: string[];
 
-  ngOnInit() {
+  optionsMulti: CalendarComponentOptions = {
+    pickMode: 'multi',
+  };
+
+  optionsRange: CalendarComponentOptions = {
+    monthFormat: 'YYYY 年 MM 月 ',
+    weekdays: ['天', '一', '二', '三', '四', '五', '六'],
+    weekStart: 1,
+    // defaultDate: {new Date()}
+  };
+
+  async calendarModal() {
+    const options: CalendarModalOptions = {
+      // pickMode: 'range',
+      pickMode: 'multi',
+      title: 'Multiple Dates',
+      monthFormat: 'YYYY 年 MM 月 ',
+      weekdays: ['日', '一', '二', '三', '四', '五', '六'],
+      weekStart: 1,
+      color: 'danger',
+      // defaultDate: {new Date()}
+    };
+
+    let calendarUi = await this.modalController.create({
+      component: CalendarModal,
+      componentProps: { options },
+    });
+
+    calendarUi.present();
   }
+
+  constructor(public modalController: ModalController) {}
+
+  
   
 }
--- src/app/home/person-list/person-list.page.ts
+++ src/app/home/person-list/person-list.page.ts
@@ -1,56 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  CalendarComponentOptions,
-  CalendarModal,
-  CalendarModalOptions,
-} from 'ion2-calendar';
-import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-person-list',
   templateUrl: './person-list.page.html',
   styleUrls: ['./person-list.page.scss'],
 })
-export class PersonListPage {
-  type: 'string'; // 'string' | 'js-date' | 'moment' | 'time' | 'object'
-  dateRange: { from: string; to: string };
-  dateMulti: string[];
-
-  optionsMulti: CalendarComponentOptions = {
-    pickMode: 'multi',
-  };
-
-  optionsRange: CalendarComponentOptions = {
-    monthFormat: 'YYYY 年 MM 月 ',
-    weekdays: ['天', '一', '二', '三', '四', '五', '六'],
-    weekStart: 1,
-    // defaultDate: {new Date()}
-  };
-
-  async calendarModal() {
-    const options: CalendarModalOptions = {
-      // pickMode: 'range',
-      pickMode: 'multi',
-      title: 'Multiple Dates',
-      monthFormat: 'YYYY 年 MM 月 ',
-      weekdays: ['日', '一', '二', '三', '四', '五', '六'],
-      weekStart: 1,
-      color: 'danger',
-      // defaultDate: {new Date()}
-    };
-
-    let calendarUi = await this.modalController.create({
-      component: CalendarModal,
-      componentProps: { options },
-    });
-
-    calendarUi.present();
-  }
-
-  constructor(public modalController: ModalController) {}
-
+export class PersonListPage implements OnInit {
   onChange($event) {
     console.log($event);
+  }
+  constructor() { }
+
+  ngOnInit() {
   }
 
   
