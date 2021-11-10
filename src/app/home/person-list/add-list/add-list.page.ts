359a0258bdb26a6b746c8f9b467939818d63580f
cfho <cfho@users.noreply.github.com>
2021-11-10 10:25:22 +0800

    get data back from calendar

--- src/app/home/person-list/add-list/add-list.page.ts
+++ src/app/home/person-list/add-list/add-list.page.ts
@@ -3,6 +3,8 @@ import {
   CalendarComponentOptions,
   CalendarModal,
   CalendarModalOptions,
+  CalendarResult,
+  DayConfig,
 } from 'ion2-calendar';
 import { ModalController } from '@ionic/angular';
 import { AfService } from 'src/app/af.service';
@@ -12,21 +14,23 @@ import { AfService } from 'src/app/af.service';
   templateUrl: './add-list.page.html',
   styleUrls: ['./add-list.page.scss'],
 })
-export class AddListPage implements OnInit{
+export class AddListPage implements OnInit {
   type: 'string'; // 'string' | 'js-date' | 'moment' | 'time' | 'object'
   dateRange: { from: string; to: string };
   dateMulti: string[];
 
-  optionsMulti: CalendarComponentOptions = {
-    pickMode: 'multi',
-  };
+  // for multi selection in component
+  // optionsMulti: CalendarComponentOptions = {
+  //   pickMode: 'multi',
+  // };
 
-  optionsRange: CalendarComponentOptions = {
-    monthFormat: 'YYYY 年 MM 月 ',
-    weekdays: ['天', '一', '二', '三', '四', '五', '六'],
-    weekStart: 1,
-    // defaultDate: {new Date()}
-  };
+  // for range selection in component
+  // optionsRange: CalendarComponentOptions = {
+  //   monthFormat: 'YYYY 年 MM 月 ',
+  //   weekdays: ['天', '一', '二', '三', '四', '五', '六'],
+  //   weekStart: 1,
+  //   // defaultDate: {new Date()}
+  // };
 
   pickUp;
   dropDown;
@@ -42,15 +46,24 @@ export class AddListPage implements OnInit{
   }
 
   async calendarModal() {
+    // let _daysConfig: DayConfig[] = [];
+    // for (let i = 0; i < 31; i++) {
+    //   _daysConfig.push({
+    //     date: new Date(2017, 0, i + 1),
+    //     subTitle: `$${i + 1}`,
+    //   });
+    // }
     const options: CalendarModalOptions = {
       // pickMode: 'range',
+      // from: new Date(2017, 0, 1),
+      // to: new Date(2017, 11.1),
+      // daysConfig: _daysConfig
       pickMode: 'multi',
       title: 'Multiple Dates',
       monthFormat: 'YYYY 年 MM 月 ',
       weekdays: ['日', '一', '二', '三', '四', '五', '六'],
       weekStart: 1,
-      color: 'danger',
-      // defaultDate: {new Date()}
+      color: 'primary',
     };
 
     let calendarUi = await this.modalController.create({
@@ -59,5 +72,10 @@ export class AddListPage implements OnInit{
     });
 
     calendarUi.present();
+
+    const event: any = await calendarUi.onDidDismiss();
+    const date: CalendarResult = event.data;
+    console.log(date);
+    console.log(event);
   }
 }
